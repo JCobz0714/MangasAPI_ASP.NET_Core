3f93c30971e81a83c937bc25d0d7f3502eb572d5
J. cob <jacoboosorio299589@gmail.com>
2025-07-08 22:03:45 -0500

    Creating the http file to communicate with the API

--- MangasAPI/obj/Debug/net9.0/MangasAPI.AssemblyInfo.cs
+++ MangasAPI/obj/Debug/net9.0/MangasAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MangasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7aa3c91f1171c11c7ea1ab8ba1a12a3885aae10")]
 [assembly: System.Reflection.AssemblyProductAttribute("MangasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MangasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
