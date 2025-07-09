6aaa21d1a8ebf0574efa4aa935e5f888023a6e49
J. cob <jacoboosorio299589@gmail.com>
2025-07-08 22:15:56 -0500

    Seeding the DTO with new data

--- MangasAPI/Program.cs
+++ MangasAPI/Program.cs
@@ -1,5 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
+
+List<MangaDto> mangas = [
+    new (1,
+        "One Piece",
+        "Eiichiro Oda",
+        "Action, Adventure, Fantasy",
+        new(1999, 7, 22)
+    ),
+    new (2,
+        "Naruto",
+        "Masashi Kishimoto",
+        "Action, Adventure, Fantasy",
+        new(2002, 9, 20)
+    ),
+    new (3,
+        "Dragon Ball",
+        "Akira Toriyama",
+        "Action, Adventure, Fantasy",
+        new(1984, 2, 29)
+    )
+];
 
 app.MapGet("/", () => "Hello World!");
 
